af2b88804301fd17ef22f19398a735fd7f5bc57c
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2022-09-12 12:19:01 +0100

    first commit

---
+++ client/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ClientsModule.register([{name: 'ITEM_MICROSERVICE', transport: Transport.TCP}])],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
